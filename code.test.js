94126624c1236da9504016ed94eeb23b14cee8d1
Nolan Berg <107907766+NolanBerg@users.noreply.github.com>
2024-02-11 13:22:08 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,21 +1,28 @@
 const fs = require('fs');
 const jsc = require('jsverify');
 
-eval(fs.readFileSync('code.js')+'');
+const { fib } = require('./code.js');
 
-function fibTest(n) {
-    var fib_solns = [0];
-    if(n == 0) return fib_solns;
-    fib_solns[1] = 1;
-    if(n == 1) return fib_solns;
-    for(var i = 2; i <= n; i++) fib_solns[i] =
-        fib_solns[i-1] + fib_solns[i-2];
-    return fib_solns;
+function arraysEqual(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
 }
 
-const test =
-    jsc.forall("nat", function(n) {
-        return JSON.stringify(fib(n)) ==
-            JSON.stringify(fibTest(n));
-    });
+const fibTest = (n) => {
+    var fib_solns = [0];
+    if (n == 0) return fib_solns;
+    fib_solns[1] = 1;
+    if (n == 1) return fib_solns;
+    for (var i = 2; i <= n; i++) fib_solns[i] =
+        fib_solns[i - 1] + fib_solns[i - 2];
+    return fib_solns;
+};
+
+const test = jsc.forall("nat", function (n) {
+    return arraysEqual(fib(n), fibTest(n));
+});
+
 jsc.assert(test);
